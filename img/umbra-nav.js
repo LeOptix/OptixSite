a57dde1335d0fc259592d51edc10496e777a5f1b
Optix <100604473+LeOptix@users.noreply.github.com>
2023-06-15 18:25:11 +0200

    Delete img directory

--- img/umbra-nav.js
+++
@@ -1,8 +0,0 @@
-    const navItems = document.querySelectorAll('.nav-item');
-        navItems.forEach(navItem => {
-            const link = navItem.querySelector('a');
-            var link2 = link.href.replace('index.html', '')
-            if(window.location.href==link.href || window.location.href==link2) {
-                navItem.classList.add("active");
-            }
-        });
